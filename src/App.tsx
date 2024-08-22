31efb8f46f3ecb665602d214cd3dcd15bcfb5b32
Esakkipandi0214 <praveenpandian01@gmail.com>
2024-08-22 13:45:29 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ import StatusVerificationTalbe from './components/Admin/StatusVerification'
 import Sidebar from './components/StaticComponents/sidebar';
 import AddMenuItem from './components/Admin/AddMenu';
 import Menu from './components/Admin/MenuView';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<ResgisterPage />} />
           <Route path="/dashbaord" element={<MainDash />}/>
           <Route path="/all-profile" element={<Profile />}/>
@@ -22,6 +22,7 @@ function App() {
           <Route path="/sidebar" element={<Sidebar />}/>
           <Route path="/add-Menu" element={<AddMenuItem />}/>
           <Route path="/View-Menu" element={<Menu />}/>
+          <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       </div>
     </BrowserRouter>
